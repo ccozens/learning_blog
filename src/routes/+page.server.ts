08b0364b4a38136722ed47d321cb21f517eb54cf
ccozens <ccozens@gmail.com>
2023-07-21 09:04:30 +0100

    tidied +page.svelte; wrote fetch req to github api

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,43 @@
+
+import { ACTION_SECRET } from '$env/static/private';
+import type { PageServerLoad } from './$types';
+
+const query = `query issues_in_progress {
+  repository(owner: "ccozens", name: "learning_blog") {
+    issues(first: 10, labels: ["in_progress"]) {
+      edges {
+        node {
+          title
+          url
+          body
+          comments(first: 10, orderBy: { field: UPDATED_AT, direction: DESC }) {
+            edges {
+              node {
+                body
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+
+export const load: PageServerLoad = async ({ fetch }) => {
+    const res = await fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `bearer ${ACTION_SECRET}`
+        },
+        body: JSON.stringify({ query })
+    });
+    const data = await res.json();
+
+
+    return {
+        issues: data
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
-<script>
+<script lang="ts">
 	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	// filter data.issues for labels = in_progress
+	const issues = data.issues;
 
 /* The data for the current session in this example was made available through the $page store which
 can be set through the root +page.server.ts file. It is not necessary to store the data there,
@@ -8,19 +14,7 @@ however, this makes it globally accessible throughout your application simplifyi
 */
 </script>
 
-<h1>SvelteKit Auth Example</h1>
-<p>
-	{#if $page.data.session}
-		{#if $page.data.session.user?.image}
-			<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
-		{/if}
-		<span class="signedInText">
-			<small>Signed in as</small><br />
-			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-		</span>
-		<button on:click={() => signOut()} class="button">Sign out</button>
-	{:else}
-		<span class="notSignedInText">You are not signed in</span>
-		<button on:click={() => signIn('github')}>Sign In with GitHub</button>
-	{/if}
-</p>
+<h1>You're ready: start doing</h1>
+
+<h2>Currently working on</h2>
+<a href={issues[0].html_url} target="_blank" >{issues[0].title}</a>
