f01b15a79f5dac5103ef4906b5f6532d609bd0b3
ccozens <ccozens@gmail.com>
2023-07-26 15:18:11 +0100

    removed accidental ./+layout.server.ts and added a tabspace rule to .prettierrc

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,5 @@
+/* This sets session data in $page store so it is available to all routes via a session object in $page.data */
+
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
