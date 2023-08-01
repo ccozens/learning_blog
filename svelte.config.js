98d68bb39630eca312b1691054eb25704309d394
ccozens <ccozens@gmail.com>
2023-08-01 13:49:36 +0100

    destructured slug in \[...slug\]/+page.server.ts to make clear that the dynamic import is in a subfolder

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,7 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import codeTitle from 'remark-code-titles';
-import remarkPresetLintConsistent from 'remark-preset-lint-consistent';
-import lintFencedCodeFlagCase from 'remark-lint-fenced-code-flag-case';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +10,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex(
 		{ extensions: ['.md'],
-		remarkPlugins: [codeTitle, remarkPresetLintConsistent, lintFencedCodeFlagCase]
+		remarkPlugins: [codeTitle]
 	})],
 	extensions: ['.svelte', '.md'],
 	kit: {
