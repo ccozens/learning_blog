719c2c68ec1b5aeba85b5c6650b58cf81957b2cf
ccozens <ccozens@gmail.com>
2023-08-04 12:49:38 +0100

    added CSS to capitalise descrption and tag display to PostListWithPreview.svelte

--- src/lib/components/PostListWithPreview.svelte
+++ src/lib/components/PostListWithPreview.svelte
@@ -1,6 +1,9 @@
 <!-- script -->
 <script lang='ts'>
     import type { AllPosts } from '$lib/types';
+	import {sortUserPlugins} from 'vite';
+	import TagCloud from './TagCloud.svelte';
+
     export let posts: AllPosts[] = [];
 </script>
 
@@ -11,10 +14,15 @@
     <article>
         <!-- note the /posts in the <a> element - makes it absolute link (/posts/slug) rather than relative (/posts/tags/posts/slug) -->
         <h1><a href="/posts/{post.slug}">{post.metadata.title}</a></h1>
-        <p>{post.metadata.description}</p>
+        <p class="preview">{post.metadata.description}</p>
         <p>Posted on {post.dateFormatted}</p>
-        <ul>
-            <!-- render tags -->
-        </ul>
+        <TagCloud tags={post.metadata.tags} />
     </article>
 {/each}
+
+
+<style>
+    .preview {
+        text-transform: capitalize;
+    }
+</style>
