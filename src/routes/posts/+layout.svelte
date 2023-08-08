f0d60a4ebb14d9c23105c6ca32db30371a12c981
ccozens <ccozens@gmail.com>
2023-08-08 14:02:28 +0100

    created layout for posts so search is always shown

---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	//  types
+	import type { PageData } from './$types';
+	//  components
+	import PostSearch from '$lib/components/search/PostSearch.svelte';
+	//  data
+	export let data: PageData;
+	const { sortedPosts } = data;
+</script>
+
+<PostSearch items={sortedPosts} placeholder="Search posts" />
+
+<slot />
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -5,14 +5,11 @@
 	import PostSearch from '$lib/components/search/PostSearch.svelte';
 	import PostListWithPreview from '$lib/components/PostListWithPreview.svelte';
 	import TagCloud from '$lib/components/TagCloud.svelte';
-	// functions
-	import { postSearch } from '$lib/functions/';
 	//  data
 	export let data: PageData;
 
 	const { sortedPosts, allTags } = data;
 </script>
 
-<PostSearch items={sortedPosts} placeholder="Search posts" />
 <PostListWithPreview posts={sortedPosts} />
 <TagCloud tags={allTags} />
