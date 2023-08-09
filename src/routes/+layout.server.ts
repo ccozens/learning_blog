42ea2ff40987beb9ded2c29130d7300061559de2
ccozens <ccozens@gmail.com>
2023-08-09 09:35:59 +0100

    escaped tags/+page@.svelte from paost layout so it doesn't show posts search and can show tags search instead. reconfigured main +layout.server.ts so posts and tags now available globally, as escaping layout meant tags/+page couldn't access data from posts/+layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,26 @@
 import type { LayoutServerLoad } from './$types';
 import { getAllPaths } from '$lib/functions/GetAllPaths';
+import { getAllTags } from '$lib/functions/GetAllTags';
+import type { Post, Tag } from '$lib/types';
 
 export const load: LayoutServerLoad = async (event) => {
-	// load path data
+	// destructure fetch function from event
+	const { fetch } = event;
+	// load path data for nav
 	const navItems = await getAllPaths();
+
+	// get all posts, sorted by date
+	const response = await fetch(`/api/posts`);
+	const sortedPosts: Post[] = await response.json();
+
+	// extract tags for tag cloud
+	const allTags: Tag[] = await getAllTags(sortedPosts);
 
 	return {
 		/* This sets session data in $page store so it is available to all routes via a session object in $page.data */
 		session: await event.locals.getSession(),
-		navItems
+		navItems,
+		allTags,
+		sortedPosts
 	};
 };
